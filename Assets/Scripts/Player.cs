67c0a7d48e77289881d12a13166db5c1ab24a95c
Alex Bezuska <abezuska@gmail.com>
2018-03-23 22:36:41 -0400

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float moveSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var moveX = moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+		var moveY = 0f;
+		var moveZ = moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+		transform.Translate(moveX, moveY, moveZ);
+	}
+}
